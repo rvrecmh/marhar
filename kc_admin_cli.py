ee20242d11dba3a78772a0eead7c5e93e4908d4c
Martin Harm <mharm@airplus.com>
2020-07-01 23:05:58 +0200

    add: local repo, hidden password entry,  - documentation adjusted

--- kc_admin_cli.py
+++ kc_admin_cli.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+import argparse
+import base64
+import getpass
 import json
 import os
+from datetime import datetime
 
 import requests
-import base64
-from datetime import datetime
-import argparse
+
 
 ## hand-made jwt ...
 def extractJwtTokenBody(jwtToken):
@@ -13,18 +15,22 @@ def extractJwtTokenBody(jwtToken):
     bodyEncoded = jwtParts[1]
     return json.loads(base64.b64decode(bodyEncoded + "==="))  ## add === to avoid padding errors
 
+
 def dumpAsTimestamp(timeInMs):
-    asDatetime=datetime.fromtimestamp(timeInMs)
+    asDatetime = datetime.fromtimestamp(timeInMs)
     return asDatetime.isoformat()
-claimFormatter= {
+
+
+claimFormatter = {
     'exp': dumpAsTimestamp,
     'iat': dumpAsTimestamp,
 }
 
-def formatJwtClaim(key,value):
-     extrFmt=claimFormatter.get(key)
-     extraInfo = ' # ' + extrFmt(value) if (extrFmt) else ''
-     return key + ':' + str(value)  + extraInfo
+
+def formatJwtClaim(key, value):
+    extrFmt = claimFormatter.get(key)
+    extraInfo = ' # ' + extrFmt(value) if (extrFmt) else ''
+    return key + ':' + str(value) + extraInfo
 
 
 #### keycloak functions
@@ -40,17 +46,20 @@ def login(loginData):
 
 ### simple helper ...
 ## env or userInput
-def envOrAsk(envVar,prompt):
+def envOrAsk(envVar, prompt, hidden=False):
     res = os.getenv(envVar)
     if res is None:
-        return input(prompt)
+        if hidden:
+            return getpass.getpass(prompt)
+        else:
+            return input(prompt)
     else:
         return res
 ##
-def valOrAsk(optionName,args,prompt):
-    val=args.__getattribute__(optionName)
+def valOrAsk(optionName, args, prompt):
+    val = args.__getattribute__(optionName)
     if val is not None:
-      return val
+        return val
     else:
         return input(prompt)
 
@@ -58,104 +67,196 @@ def valOrAsk(optionName,args,prompt):
 ###  define the args
 def defineMainParser():
     parser = argparse.ArgumentParser(description='keycloak tiny admin')
-    parser.add_argument('--base', help='Base url of keycloak', default='http://localhost:8080/auth')
+    parser.add_argument('--base', help='Base url of keycloak',
+                        default=os.getenv('KEYCLOAK_BASE', 'http://localhost:8080/auth'))
+    parser.add_argument('--repo', help='Path to the local repo', default='./repo')
     parser.add_argument('cmd', help='the command to use')
     parser.add_argument('--realm', help='id of the realm to use')
     parser.add_argument('--displayName', help='used when creating a realm')
     parser.add_argument('--client', help='id of the client to use')
     parser.add_argument('--url', help='used as redirectUrl when cmd=addRedirect')
+    parser.add_argument('--override', help='flag, on create, to override the state in the repo')
 
     return parser
+
 
 ### main logic
 args = defineMainParser().parse_args()
 
 ## extract args ..
-keycloakBase= args.base
+keycloakBase = args.base
 ## build login info
 keycloakAdminLogin = {
     'client_id': 'admin-cli',
     'grant_type': 'password',
-    'username': envOrAsk('KEYCLOAK_USER','Keycloak user:'),
-    'password': envOrAsk('KEYCLOAK_PWD','Keycloak password:'),
+    'username': envOrAsk('KEYCLOAK_USER', 'Keycloak user:'),
+    'password': envOrAsk('KEYCLOAK_PWD', 'Keycloak password:', hidden=True),
     'scope': 'openid'
 }
 ### global var ...
-loginData=login(keycloakAdminLogin)
+loginData = login(keycloakAdminLogin)
 headers = {
     'content-type': 'application/json',
     'Authorization': 'Bearer ' + loginData['access_token']
 }
 
-## dump id token
+
+##
+
+
+class RepoStorage:
+    def __init__(self, baseDir):
+        # TODO: add the baseURL as a subdirectory
+        self.baseDir = baseDir
+
+    def getClientDir(self, realm):
+        return self.baseDir + "/" + realm + "/clients/"
+
+    def getClientFile(self, realm, client):
+        return self.getClientDir(realm) + client + ".json"
+
+    def saveClient(self, realm, clientData):
+        """Store the clientData at realm/clientData[clientId]"""
+        filename = self.getClientFile(realm, clientData['clientId'])
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w') as db:
+            json.dump(clientData, fp=db, sort_keys=True, indent=2)
+
+    def clientExists(self, realm, client):
+        filename = self.getClientFile(realm, client)
+        return os.path.exists(filename)
+
+    def loadClient(self, realm, client):
+        filename = self.getClientFile(realm, client)
+        with open(filename, 'r') as db:
+            return json.load(db)
+
+    def loadClients(self, realm):
+        dirname = self.getClientDir(realm)
+        if os.path.exists(dirname):
+            clientFiles = [f for f in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, f))]
+            for cf in clientFiles:
+                with open(os.path.join(dirname, cf), 'r') as db:
+                    yield json.load(db)
+
 
 ###
 class CmdHandler:
+    def __init__(self, repo):
+        self.repo = repo
+
     def callCmd(self, args):
         method = getattr(self, args.cmd, lambda: "Unknown cmd")
         return method(args)
 
-    def dumpIdToken(self,args):
-            jwtToken = extractJwtTokenBody(loginData['id_token'])
-            print('id_token:')
-            for claim in sorted(jwtToken.items()):
-                print('  ' + formatJwtClaim(claim[0], claim[1]))
-    def createRealm(self,args):
-        payload={
-             'id': args.realm,
+    def dumpIdToken(self, args):
+        jwtToken = extractJwtTokenBody(loginData['id_token'])
+        print('id_token:')
+        for claim in sorted(jwtToken.items()):
+            print('  ' + formatJwtClaim(claim[0], claim[1]))
+
+    def createRealm(self, args):
+        payload = {
+            'id': args.realm,
             'realm': args.realm,
-            'displayName': valOrAsk('displayName',args,'Display name:')
+            'displayName': valOrAsk('displayName', args, 'Display name:')
         }
-        resp=requests.post(keycloakBase + '/admin/realms', headers=headers, json=payload)
+        resp = requests.post(keycloakBase + '/admin/realms', headers=headers, json=payload)
         resp.raise_for_status()
-    def deleteRealm(self,args):
-        resp=requests.delete(keycloakBase + '/admin/realms/' + args.realm, headers=headers)
-        resp.raise_for_status()
-    def listClients(self, args):
+        print('# realm created:' + args.realm)
 
+    def deleteRealm(self, args):
+        resp = requests.delete(keycloakBase + '/admin/realms/' + args.realm, headers=headers)
+        resp.raise_for_status()
+        print('# realm deleted:' + args.realm)
+
+    def checkClients(self, args):
         resp = requests.get(keycloakBase + '/admin/realms/' + args.realm + "/clients", headers=headers,
-                            params={'clientId':args.client})
+                            params={'clientId': args.client})
         resp.raise_for_status()
-        clientsFound=json.loads(resp.content)
-        if not clientsFound:
-            print("# No client(s) found")
-            exit(-1)
-        else:
-            for cc in json.loads(resp.content):
-                print(cc)
+        byId = lambda obj: [obj['id'], obj]
+        keycloakClientsById = dict(map(byId, json.loads(resp.content)))
+        repoClientsById = dict(map(byId, self.repo.loadClients(args.realm)))
+        allClients = set(keycloakClientsById.keys()) | set(repoClientsById.keys())
+        print("Clients of realm: " + args.realm + " compared to local repo")
+        for client in sorted(allClients):
+            inRepo = repoClientsById.get(client)
+            inKeycloak = keycloakClientsById.get(client)
+            status = None
+            if inRepo and not inKeycloak: status = 'missing'
+            if not inRepo and inKeycloak: status = 'stale'
+            if inRepo and inKeycloak:
+                status = 'equals' if inRepo == inKeycloak else 'differs'
+            print(client + " : " + status)
 
-    def createClient(self,args):
+    def createClient(self, args):
+
+        # TODO: Use a template, like in ansible??
         payload = {
             'id': args.client,
             'publicClient': False
         }
-        resp=requests.post(keycloakBase + '/admin/realms/' + args.realm + "/clients", headers=headers, json=payload )
+        if self.repo.clientExists(args.realm, args.client) and not args.override:
+            print('# client ' + args.client + ' exists in local repo.')
+            exit(-1)
+
+        resp = requests.post(keycloakBase + '/admin/realms/' + args.realm + "/clients", headers=headers, json=payload)
         resp.raise_for_status()
-    def deleteClient(self,args):
-        resp=requests.delete(keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client, headers=headers )
+        self.fetchAndStoreClient(args.realm, args.client)
+        print('# client created:' + args.realm + '/' + args.client)
+
+    def restoreClient(self, args):
+        if not self.repo.clientExists(args.realm, args.client):
+            print('# client ' + args.client + ' does not exists in local repo.')
+            exit(-1)
+        clientUrl = keycloakBase + '/admin/realms/' + args.realm + "/clients/"
+        clientData = self.repo.loadClient(args.realm, args.client)
+        resp = requests.post(clientUrl, headers=headers, json=clientData)
         resp.raise_for_status()
+        self.fetchAndStoreClient(args.realm, args.client)
+        print('# client:' + args.realm + '/' + args.client + ': restored from repo')
+
+    def fetchAndStoreClient(self, realm, client):
+        resp = requests.get(keycloakBase + '/admin/realms/' + realm + "/clients/" + client, headers=headers)
+        resp.raise_for_status()
+        self.repo.saveClient(realm, json.loads(resp.content))
+
+    def deleteClient(self, args):
+        resp = requests.delete(keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client,
+                               headers=headers)
+        resp.raise_for_status()
+        print('# client deleted:' + args.realm + '/' + args.client)
 
     def dumpClient(self, args):
         resp = requests.get(keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client,
-                               headers=headers)
+                            headers=headers)
         resp.raise_for_status()
-        print(resp.content)
+        print(json.dumps(json.loads(resp.content), indent=2, sort_keys=True))
 
-    def addRedirect(self,args):
-        clientUrl=keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client
+    def dumpClientSecret(self, args):
+        resp = requests.get(keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client + "/client-secret",
+                            headers=headers)
+        resp.raise_for_status()
+        clientSecret = json.loads(resp.content)
+        print('# client secret:' + args.realm + '/' + args.client + ': ' + clientSecret.get('value'))
+
+    def addRedirect(self, args):
+        clientUrl = keycloakBase + '/admin/realms/' + args.realm + "/clients/" + args.client
         resp = requests.get(clientUrl, headers=headers)
         resp.raise_for_status()
         ## do the merge
-        oldRedirects=set(json.loads(resp.content).get('redirectUris'))
-        newRedirect=valOrAsk('url',args,'RedirectURL:')
+        oldRedirects = set(json.loads(resp.content).get('redirectUris'))
+        newRedirect = valOrAsk('url', args, 'RedirectURL:')
         if newRedirect not in oldRedirects:
-            newRedirects=set(oldRedirects)
+            newRedirects = set(oldRedirects)
             newRedirects.add(newRedirect)
             payload = {
                 'redirectUris': list(newRedirects)
             }
-            resp=requests.put(clientUrl, headers=headers ,json=payload )
+            resp = requests.put(clientUrl, headers=headers, json=payload)
             resp.raise_for_status()
+            self.fetchAndStoreClient(args.realm, args.client)
+            print('# client updated:' + args.realm + '/' + args.client + ': redirect added:' + newRedirect)
 
     def listRealms(self, args):
         resp = requests.get(keycloakBase + '/admin/realms', headers=headers)
@@ -164,5 +265,6 @@ class CmdHandler:
             print(realm['realm'])
 
 
+repo = RepoStorage(args.repo)
 
-CmdHandler().callCmd(args)
+CmdHandler(repo).callCmd(args)
--- tasks.md
+++ tasks.md
@@ -6,11 +6,15 @@ all tasks are done, by calling the kc_admin_cli.py with some parameters:
 ### Credentials
 As I won't store credentials, the programm will accept them via environment variables:
 ```shell script
-KEYCLOAK_USER=admin
-KEYCLOAK_PWD=admin
+export KEYCLOAK_USER=admin
+export KEYCLOAK_PWD=admin
 ```
 As a fallback, they are asked from  the user.
- 
+
+Also the base url could be set via environment.
+```shell script
+export KEYCLOAK_BASE=http://localhost:8080/auth 
+``` 
 
 #### 1. dump the id token
 
@@ -19,56 +23,78 @@ As a fallback, they are asked from  the user.
 #### 2. create three realms and a client in each
 
 ```
-kc_admin_cli.py createRealm --realm one   
-kc_admin_cli.py  --realm one createClient --client=myClient
+./kc_admin_cli.py createRealm --realm one   
+./kc_admin_cli.py  --realm one createClient --client=myClient
 
-kc_admin_cli.py createRealm --realm two --displayName "Second demo realm"
-kc_admin_cli.py  --realm two createClient --client=anotherClient
+./kc_admin_cli.py createRealm --realm two --displayName "Second demo realm"
+./kc_admin_cli.py  --realm two createClient --client=anotherClient
 
-kc_admin_cli.py createRealm --realm three --displayName "Third demo realm"  
-kc_admin_cli.py  --realm three createClient --client=yac
+./kc_admin_cli.py createRealm --realm three --displayName "Third demo realm"  
+./kc_admin_cli.py  --realm three createClient --client=yac
 ```
+
+To get the clientSecret of a client call
+`./kc_admin_cli.py --realm one dumpClientSecret --client myClient`
 
 #### 3. add a redirect URL to one client
 
-`kc_admin_cli.py addRedirect --realm one --client myClient  --url http://over.the.rainbow`
+`./kc_admin_cli.py  --realm one addRedirect --client myClient  --url http://over.the.rainbow`
 
 #### 4. delete one client
 
-`kc_admin_cli.py --realm one deleteClient --client=myClient`
+`./kc_admin_cli.py --realm one deleteClient --client=myClient`
  
 #### 5a. manual check the clients
 
 Query for the client:
-`kc_admin_cli.py --realm one listClients --client=myClient`
+`./kc_admin_cli.py --realm one listClients --client=myClient`
 
 If "# No client(s) found", than the client is gone.
 
 To recreate, all the calls, eg. like
 ```shell script
-kc_admin_cli.py  --realm one createClient --client=myClient
-kc_admin_cli.py addRedirect --realm one --client myClient  --url http://over.the.rainbow
+./kc_admin_cli.py  --realm one createClient --client=myClient
+./kc_admin_cli.py addRedirect --realm one --client myClient  --url http://over.the.rainbow
 ``` 
 
 needs to be performed. 
  
 #### 5b. keep a state (of clients only)
 
-**NYI**
-
-The task "Detect that one of the clients is missing" , could mean, that we should be able 
-to detect that we need to know the expected clients **without** providing this as parameter. 
+The task "Detect that one of the clients is missing", could mean, that we should be able 
+to detect the absence of a client in keycloak, that has been deleted by accident **without** providing 
+the clientName as parameter. 
  
-`kc_admin_cli.py --realm one --checkClients `
+`./kc_admin_cli.py --realm one checkClients `
 
-To "and add it again" than should mean, that we recreate the client, with the initial provided infos:
+All clients with status: missing are in our local repo (could be used as a git repo) but not in 
+keycloak. To "and add it again" recreate the client, with the last know status:
 
-`kc_admin_cli.py --realm one --client myClient --restoreClient`
+`./kc_admin_cli.py --realm one --client myClient restoreClient`
+
+Note: some infos of the client are currently missing, eg. the client-roles, as they are not returned 
+(directly) from the keycloak-admin-api.
+   
+BUT at least the clientSecret has to be treated special.
+
+
+### Leftovers
+
+The current design, all in one file, is not a good idea. It should be split in python modules.
+The hand-crafted access on the keycloak-admi-api should be replaced by the usage of an existing
+module for that. 
+
+Error handling is currently not done at all. At least some more checks on user input should be added.
+
+Complete all the infos of clients, like roles, and add a way to store the clientCredentials, especially 
+the clientSecret (Maybe encrypted with the 'KEYCLOAK_PWD')
+
+And of course: no tests included. 
+
+
+Martin Harm 
   
-
- 
-
- 
+   
 
 
 
